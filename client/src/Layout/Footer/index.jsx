0dbb99804fc279b9b38bc5cc8802c65883198a6a
unknown <a0548428751@gmail.com>
2024-12-18 11:03:46 +0200

    connect

--- client/src/Layout/Footer/index.jsx
+++ client/src/Layout/Footer/index.jsx
@@ -1,5 +1,6 @@
 import styles from './style.module.css';
 import { useNavigate } from 'react-router-dom';
+import Popup from 'reactjs-popup';
 
 export default function Footer() {
   const navigate = useNavigate();
@@ -7,15 +8,19 @@ export default function Footer() {
   return (
     <div className={styles.footer}>
       <div>
-        <a href='#' className={styles.a}>  גלריה  </a>
-        <a href='#' className={styles.a}> | חדרי המלון  </a>
-        <a href='#' className={styles.a} onClick={() => navigate('/myBookings')}> | ההזמנות שלי  |</a>
-        <a href='#' className={styles.a}>   צור קשר  </a>
-
-
-
-
+        <a href='#' className={styles.a} onClick={() => navigate('/gallery')}> גלריה </a>
+        <a href='#' className={styles.a}> | חדרי המלון </a>
+        <a href='#' className={styles.a} onClick={() => navigate('/myBookings')}> | ההזמנות שלי | </a>
+        
+        <Popup trigger={<a className={styles.a}>צור קשר</a>} position="right center" closeOnDocumentClick>
+          <div className={styles.popupContent}>
+            <h3>פרטי המלון</h3>
+            <p>כתובת: זריצקי דוד 3 ירושלים</p>
+            <p>טלפון: 0779630866</p>
+            <p> contact@hotel.com :דוא"ל </p>
+          </div>
+        </Popup>
       </div>
     </div>
-  )
+  );
 }
--- client/src/components/Menu/index.jsx
+++ client/src/components/Menu/index.jsx
@@ -1,5 +1,7 @@
 import styles from './style.module.css';
 import { useNavigate } from 'react-router-dom';
+import Popup from 'reactjs-popup';
+
 export default function Menu() {
   const navigate = useNavigate();
   
@@ -8,11 +10,17 @@ export default function Menu() {
       <h3>🌟🌟🌟🌟🌟</h3>
       <h1 style={{ cursor: 'pointer' }} onClick={() => navigate('/')}>wellcome to the hotel</h1>
       <div>
-        <a href='#gallery' className={styles.a}>  גלריה  </a>
+        <a href='#' className={styles.a} onClick={() => navigate('/gallery')}> גלריה </a>
         <a href='#' onClick={() => navigate('/rooms')} className={styles.a}> | חדרי המלון  </a>
-        <a href='#' className={styles.a} onClick={() => navigate('/myBookings')}> | ההזמנות שלי  |</a>
-        <a href='#' className={styles.a}>   צור קשר  </a>
-
+        <a href='#' className={styles.a} onClick={() => navigate('/myBookings')}> | ההזמנות שלי  | </a>
+        <Popup trigger={<a className={styles.a}>צור קשר</a>} position="right center" closeOnDocumentClick>
+          <div className={styles.popupContent}>
+            <h3>פרטי המלון</h3>
+            <p>כתובת: זריצקי דוד 3 ירושלים</p>
+            <p>טלפון: 0779630866</p>
+            <p> contact@hotel.com :דוא"ל </p>
+          </div>
+        </Popup>
 
 
       </div>
